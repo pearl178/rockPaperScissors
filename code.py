be05e12565f840a8186a90068c14902d133f7165
pearl178 <89487585+pearl178@users.noreply.github.com>
2022-04-18 22:08:59 -0600

    add input control

--- code.py
+++ code.py
@@ -29,30 +29,33 @@ scissors = '''
 import random
 choices = [rock,paper,scissors]
 num_user = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
-user_choice = choices[num_user]
-print(user_choice)
-num_computer = random.randint(0,2)
-computer_choice = choices[num_computer]
-print("Computer chose:")
-print(computer_choice)
-if num_user == 0:
-  if num_computer == 0:
-    print("It's a draw.")
-  elif num_computer == 1:
-    print("You lose.")
-  else:
-    print("You won!")
-if num_user == 1:
-  if num_computer == 0:
-    print("You won!")
-  elif num_computer == 1:
-    print("It's a draw.")
-  else:
-    print("You lose.")
-if num_user == 2:
-  if num_computer == 0:
-    print("You lose.")
-  elif num_computer == 1:
-    print("You won!")
-  else:
-    print("It's a draw.")
+if num_user<0 or num_user>2:
+  print("You entered an invalid number")
+else:
+ user_choice = choices[num_user]
+ print(user_choice)
+ num_computer = random.randint(0,2)
+ computer_choice = choices[num_computer]
+ print("Computer chose:")
+ print(computer_choice)
+ if num_user == 0:
+   if num_computer == 0:
+     print("It's a draw.")
+   elif num_computer == 1:
+     print("You lose.")
+   else:
+     print("You won!")
+ if num_user == 1:
+   if num_computer == 0:
+     print("You won!")
+   elif num_computer == 1:
+     print("It's a draw.")
+   else:
+     print("You lose.")
+ if num_user == 2:
+   if num_computer == 0:
+     print("You lose.")
+   elif num_computer == 1:
+     print("You won!")
+   else:
+     print("It's a draw.")
